60906be8f6ab7186ebff05a888f281484686a38a
perry1224 <perry.w1224@gmail.com>
2023-04-06 23:36:56 -0400

    updated submit button

--- scripts.js
+++ scripts.js
@@ -26,3 +26,7 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+function enableBtn(){
+  document.getElementById("submit1").disabled = false;
+}
