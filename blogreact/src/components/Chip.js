42e250593e1d5fb4e7b72d8bc0897676827a4a62
perry1224 <perry.w1224@gmail.com>
2023-04-08 00:11:37 -0400

    removed blog

--- blogreact/src/components/Chip.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import '../index.css';
-const Chip = ({ label }) => <p className='chip'>{label}</p>;
-export default Chip;
--- blogreact/src/components/EmptyList.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../index.css';
-const EmptyList = () => (
-  <div className='emptyList-wrap'>
-    <img src='/assets/13525-empty.gif' alt='empty' />
-  </div>
-);
-export default EmptyList;
